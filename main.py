67cc087daccd3705eb1cfe4092ecdf6e05968870
Dillon Barnes <83948303+DillonB07@users.noreply.github.com>
2021-12-01 17:47:08 +0000

    Disable debug mode

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ from website import create_app
 
 if __name__ == '__main__':
     app = create_app()
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=False)
